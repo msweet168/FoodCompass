a7fe761f4b98ed3aa9f01f4b04ff4e5d3def44ab
Nicholas Pascucci <npascuc2@u.rochester.edu>
2020-12-03 21:17:09 -0500

    Initial Commit

---
+++ FoodCompass/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodCompass
+//
+//  Created by Nicholas Pascucci on 12/3/20.
+//  Copyright © 2020 Nicholas Pascucci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
